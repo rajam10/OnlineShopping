661cb351426031a5ad7bceec2edbdcc317ea423f
rajam10 <rajabalan6300@email.com>
2026-02-09 11:41:55 +0530

    Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import './App.css'
 import { Routes } from "react-router"
 import LoginPage from './pages/loginPage.jsx'
-import ProductList from './pages/ProductList.jsx'
 
 function App() {
- 
   return (
     <>
       <LoginPage />
